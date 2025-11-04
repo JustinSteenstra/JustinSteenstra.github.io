37c285fcc7b85600e151764d4539c5673631d85e
Justin Steenstra <justin.c.steenstra@gmail.com>
2025-11-04 09:07:49 -0500

    11.4.25

--- js/main.js
+++ js/main.js
@@ -118,14 +118,12 @@
     /*----------------------------------------------------*/
     /*	Modal Popup
     ------------------------------------------------------*/
-    $('.item-wrap a').magnificPopup({
-
-        type: 'inline',
-        fixedContentPos: false,
-        removalDelay: 300,
-        showCloseBtn: false,
-        mainClass: 'mfp-fade'
-
+   $('.item-wrap a[href^="#"]').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    removalDelay: 300,
+    showCloseBtn: false,
+    mainClass: 'mfp-fade'
     });
 
     $(document).on('click', '.popup-modal-dismiss', function(e) {
