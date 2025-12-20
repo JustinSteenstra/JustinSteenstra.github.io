bcd47b0d272198640cdab124916d949562bc95e9
JustinSteenstra <justin.c.steenstra@gmail.com>
2025-12-19 20:16:10 -0500

    12.19/20

--- js/main.js
+++ js/main.js
@@ -99,6 +99,7 @@
 
     });
 
+  
 
     /*---------------------------------------------------- */
     /*	Masonry
@@ -138,24 +139,7 @@
     var toggleButton = $('.menu-toggle'),
         nav = $('.main-navigation');
 
-    // toggle button
-    toggleButton.on('click', function(e) {
-
-        e.preventDefault();
-        toggleButton.toggleClass('is-clicked');
-        nav.slideToggle();
-
-    });
-
-    // nav items
-    nav.find('li a').on("click", function() {
-
-        // update the toggle button 		
-        toggleButton.toggleClass('is-clicked');
-        // fadeout the navigation panel
-        nav.fadeOut();
-
-    });
+    
 
 
     /*---------------------------------------------------- */
